6220c75a638e5686df1033ba48f245e1247e2aab
4hoeschele <Carsten Hoeschele>
2024-07-13 20:35:40 +0200

    [main IMPROVE] Added backend for dnd character creation and logging

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"4hoeschele/go_dnd_project.git438/cmd/api"
-	"4hoeschele/go_dnd_project.git438/db"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/go-sql-driver/mysql"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+    "github.com/gin-contrib/cors"
 )
+
+type Character struct {
+	ID		 string    `json:"id"`
+    Name       string `json:"name"`
+    Class      string `json:"class"`
+    Race       string `json:"race"`
+    Strength   int    `json:"strength"`
+    Dexterity  int    `json:"dexterity"`
+    Constitution int  `json:"constitution"`
+    Intelligence int  `json:"intelligence"`
+    Wisdom      int   `json:"wisdom"`
+    Charisma    int   `json:"charisma"`
+}
+
+// create dummy for database
+var characters = []Character{
+	{ID: "1", Name: "Aragorn", Class: "Ranger", Race: "Human", Strength: 15, Dexterity: 14, Constitution: 13, Intelligence: 12, Wisdom: 10, Charisma: 8},
+	{ID: "2", Name: "Legolas", Class: "Archer", Race: "Elf", Strength: 14, Dexterity: 18, Constitution: 12, Intelligence: 11, Wisdom: 13, Charisma: 9},
+	{ID: "3", Name: "Gimli", Class: "Warrior", Race: "Dwarf", Strength: 16, Dexterity: 12, Constitution: 15, Intelligence: 10, Wisdom: 9, Charisma: 7},
+}
 
 func init() {
 		err := godotenv.Load()
@@ -19,33 +39,53 @@ func init() {
 	fmt.Println("Server is successfully running on port:", os.Getenv("PORT"))
 }
 
-func main() {
-	dbUser := os.Getenv("DB_USER")
-    dbPass := os.Getenv("DB_PASS")
-    dbAddr := os.Getenv("DB_ADDR")
-    dbName := os.Getenv("DB_NAME")
+func getCharacters(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, characters)
+}
 
-    db, err := db.NewMySQLStorage(mysql.Config{
-        User:                 dbUser,
-        Passwd:               dbPass,
-        Addr:                 dbAddr,
-        DBName:               dbName,
-        Net:                  "tcp",
-        AllowNativePasswords: true,
-        ParseTime:            true,
-    })
+func getCharactersBy(c *gin.Context) {
+	// get id from url param
+	id := c.Param("id")
+	// grab the character by id
+	char, err := getCharacterByIdHelper(id)
+
 	if err != nil {
-		fmt.Println(err)
-		fmt.Println(db)
-	}
-    fmt.Println("Database Username: ", dbUser)
-	port := os.Getenv("PORT")
-	router := api.InitRoutes()
-	// creates a new server instans
-	server := &http.Server{
-		Addr: ":" + port,
-		Handler: router,
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Character not found"})
+		return
 	}
 
-	server.ListenAndServe()
+	c.IndentedJSON(http.StatusOK, char)
+}
+
+func getCharacterByIdHelper(id string) (*Character, error) {
+	for i, char := range characters {
+		if char.ID == id {
+			return &characters[i], nil
+		}
+	}
+	return nil, errors.New("Character not found")
+}
+
+func createCharacter(c *gin.Context) {
+	var newCharacter Character
+	// bind the request body to newCharacter
+	if err := c.BindJSON(&newCharacter); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid request"})
+		return
+	}
+
+	// append newCharacter to characters
+	characters = append(characters, newCharacter)
+	c.IndentedJSON(http.StatusCreated, newCharacter)
+}
+
+func main() {
+	// setup server
+	router := gin.Default()
+	router.Use(cors.Default())
+	router.GET("/characters", getCharacters)
+	router.GET("/characters/:id", getCharactersBy)
+	router.POST("/characters", createCharacter)
+
+	router.Run("localhost:8080")
 }
